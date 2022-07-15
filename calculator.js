127c6de9cbffd73a80bda4559d9807154a058ce2
Owusu-Desmond <desmond0406114009@gmail.com>
2022-07-15 09:57:42 +0000

    Create calculator class

---
+++ calculator.js
@@ -0,0 +1,20 @@
+class Calculator {
+    constructor(num1, num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    add(){
+        return this.num1 + this.num2;
+    }
+
+    subtract(){
+        return Math.abs(this.num1 - this.num2);
+    }
+
+    divide(){
+        return this.num1 / this.num2;
+    }
+}
+
+module.exports = Calculator;
